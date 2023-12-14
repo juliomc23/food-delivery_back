ebff8784a5bc36c65e805713c5daf2e5476358b3
julio <julito2302mc@gmail.com>
2023-12-14 17:34:19 +0100

    feat: changes made in food resource

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -6,10 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { FoodsService } from './foods.service';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { UpdateFoodDto } from './dto/update-food.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('foods')
 export class FoodsController {
@@ -21,22 +24,25 @@ export class FoodsController {
   }
 
   @Get()
-  findAll() {
-    return this.foodsService.findAll();
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.foodsService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.foodsService.findOne(id);
+  @Get(':param')
+  findOne(@Param('param') param: string) {
+    return this.foodsService.findOne(param);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFoodDto: UpdateFoodDto) {
-    return this.foodsService.update(+id, updateFoodDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateFoodDto: UpdateFoodDto,
+  ) {
+    return this.foodsService.update(id, updateFoodDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.foodsService.remove(id);
   }
 }
--- src/foods/foods.service.ts
+++ src/foods/foods.service.ts
@@ -8,6 +8,8 @@ import { UpdateFoodDto } from './dto/update-food.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Food } from './entities/food.entity';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { validate as isUuid } from 'uuid';
 
 @Injectable()
 export class FoodsService {
@@ -24,19 +26,45 @@ export class FoodsService {
     }
   }
 
-  async findAll() {
-    return await this.foodRepository.find();
+  async findAll(pagination: PaginationDto) {
+    const { limit = 7, page = 0 } = pagination;
+    return await this.foodRepository.find({
+      take: limit,
+      skip: page,
+    });
   }
 
-  async findOne(id: string) {
-    const findedFood = await this.foodRepository.findOneBy({ id });
+  async findOne(param: string) {
+    let findedFood: Food;
+
+    if (isUuid(param)) {
+      // Si es un UUID válido, busca por id
+      findedFood = await this.foodRepository.findOneBy({ id: param });
+    } else {
+      // Si no es un UUID, busca por food_name
+      const queryBuilder = this.foodRepository.createQueryBuilder();
+      findedFood = await queryBuilder
+        .where('food_name = :food_name', { food_name: param })
+        .getOne();
+    }
+
     if (!findedFood)
-      throw new NotFoundException(`The food with id ${id} was not found`);
+      throw new NotFoundException(`The food ${param} was not found`);
+
     return findedFood;
   }
 
-  update(id: number, _updateFoodDto: UpdateFoodDto) {
-    return `This action updates a #${id} food`;
+  async update(id: string, updateFoodDto: UpdateFoodDto) {
+    const food = await this.foodRepository.preload({
+      id,
+      ...updateFoodDto,
+    });
+
+    if (!food)
+      throw new NotFoundException(`The food with id: ${id} was not found`);
+
+    await this.foodRepository.save(food);
+    return food;
   }
 
   async remove(id: string) {
