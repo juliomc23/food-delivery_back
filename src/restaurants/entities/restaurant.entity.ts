a6d3bc6850f6b78f4bab09efa98b042d955f4f51
julio <julito2302mc@gmail.com>
2023-12-19 18:50:17 +0100

    feat: added restaurant basic entity
    In this new feature, basic restaurant entity logic was created and added to restaurant module and food entity
    
    restaurant now is a one to one relation

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,1 +1,17 @@
-export class Restaurant {}
+import { Food } from 'src/foods/entities';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'restaurants' })
+export class Restaurant {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Food, (food) => food.restaurant, { cascade: true })
+  foods: Food[];
+
+  @Column()
+  home_delivery: boolean;
+}
