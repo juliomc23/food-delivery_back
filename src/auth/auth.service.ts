984db284b39809f07585949dea5c549a5dbc63ab
julio <julito2302mc@gmail.com>
2023-12-21 14:30:59 +0100

    feat: added signup logic
    this new feature, added signup logic in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { SignUpDto } from './dto/signup.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/auth.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(signupDto: SignUpDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(signupDto: SignUpDto) {
+    const hashedPassword = this.hashPassword(signupDto.password);
+    try {
+      const user = this.userRepository.create({
+        ...signupDto,
+        password: hashedPassword,
+      });
+      await this.userRepository.save(user);
+
+      return user;
+    } catch (error) {
+      this.handleDbErrors(error);
+    }
+  }
+
+  private handleDbErrors(error: any): void {
+    if (error.code === '23505') {
+      throw new ConflictException(error.detail);
+    }
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
+  }
+
+  private hashPassword(password: string) {
+    const salt = bcrypt.genSaltSync();
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    return hashedPassword;
   }
 }
