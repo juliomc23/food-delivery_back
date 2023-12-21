158814a6b8851e8232f89946c8240a0515f21a90
julio <julito2302mc@gmail.com>
2023-12-21 10:28:31 +0100

    feat: added colums to User entity
    This new feature provides User entity fields and provides TypeOrm module to auth module

--- src/auth/entities/auth.entity.ts
+++ src/auth/entities/auth.entity.ts
@@ -1,1 +1,21 @@
-export class Auth {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'text' })
+  name: string;
+  @Column({ type: 'text' })
+  lastname: string;
+  @Column({ type: 'text', unique: true })
+  email: string;
+  @Column({ type: 'text' })
+  password: string;
+  @Column({ type: 'text' })
+  birthdate: string;
+  @Column({ type: 'boolean' })
+  isActive: boolean;
+  @Column({ type: 'text', array: true, default: ['user'] })
+  roles: string[];
+}
