acf1f36d77337f26c29c28586db6500234068e1f
julio <julito2302mc@gmail.com>
2023-12-21 16:41:32 +0100

    feat: added jwt module and passport module
    In this new feature, Passport and Jwt module was added to auth.module with his own configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,27 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/auth.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.getOrThrow('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
